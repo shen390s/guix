168d6ebc4f78e39e4ecdd403beda50974d48ce0e
Arun Isaac <arunisaac@systemreboot.net>
2020-09-16 10:59:54 +0530

    gnu: Add rust-cloudabi-0.1.
    * gnu/packages/crates-io.scm (rust-cloudabi-0.1): New variable.
    (rust-cloudabi-0.0): Inherit from rust-cloudabi-0.1.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -3587,28 +3587,42 @@ colorization.")
 pitfalls in Rust.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-cloudabi-0.1
+  (package
+    (name "rust-cloudabi")
+    (version "0.1.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "cloudabi" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "0rv4yf5jlldfkynzrw687s00f4x12ypw7axv71vawhy6h4i52i23"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-bitflags" ,rust-bitflags-1))))
+    (home-page "https://cloudabi.org/")
+    (synopsis "Low level interface to CloudABI")
+    (description "This package provides a low level interface to CloudABI.  It
+contains all syscalls and related types.")
+    (license license:bsd-2)))
+
 (define-public rust-cloudabi-0.0
   (package
+    (inherit rust-cloudabi-0.1)
     (name "rust-cloudabi")
     (version "0.0.3")
     (source
-      (origin
-        (method url-fetch)
-        (uri (crate-uri "cloudabi" version))
-        (file-name (string-append name "-" version ".crate"))
-        (sha256
-         (base32
-          "0kxcg83jlihy0phnd2g8c2c303px3l2p3pkjz357ll6llnd5pz6x"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:skip-build? #t
-       #:cargo-inputs
-       (("rust-bitflags" ,rust-bitflags-1))))
-    (home-page "https://nuxi.nl/cloudabi/")
-    (synopsis "Low level interface to CloudABI")
-    (description
-     "Low level interface to CloudABI.  Contains all syscalls and related types.")
-    (license license:bsd-2)))
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "cloudabi" version))
+       (file-name (string-append name "-" version ".crate"))
+       (sha256
+        (base32
+         "0kxcg83jlihy0phnd2g8c2c303px3l2p3pkjz357ll6llnd5pz6x"))))))
 
 (define-public rust-cloudflare-zlib-sys-0.2
   (package
